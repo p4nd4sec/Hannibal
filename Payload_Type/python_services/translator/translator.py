ade570d291f1106ef2d14344a6a2b67859d0fe8e
overkillsec <overkillsec_email@proton.me>
2024-12-17 20:41:33 -0800

    Fixed missing return statement in translator. Caused Mythic to complain about PKCS7 issues when responding to completed tasks to agent.

--- Payload_Type/python_services/translator/translator.py
+++ Payload_Type/python_services/translator/translator.py
@@ -467,6 +467,8 @@ class hannibal_python_translator(TranslationContainer):
             data += struct.pack('<B', 1)
         else:
             data += struct.pack('<B', 0)
+        
+        return data
 
     def serialize_init_file_download(self, json_object):
 
