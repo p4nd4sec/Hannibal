8382c86d2e066938a43a9b94c6cd64744f69ab7f
h114mx001 <21anh.nt@vinuni.edu.vn>
2025-04-17 12:21:29 +0700

    update

--- Payload_Type/hannibal/hannibal/mythic/agent_functions/execute_bof.py
+++ Payload_Type/hannibal/hannibal/mythic/agent_functions/execute_bof.py
@@ -123,22 +123,21 @@ class ExecuteBofCommand(CommandBase):
         fData_additional = FileData()
         
         fData_additional.AgentFileId = taskData.args.get_arg("additional_file")
-        try:
-            if fData_additional.AgentFileId is not None:
-                file_additional = await SendMythicRPCFileGetContent(fData_additional)
+      
+        if fData_additional.AgentFileId is not None:
+            file_additional = await SendMythicRPCFileGetContent(fData_additional)
 
-                if file_additional.Success:
-                    if (len(file_additional.Content) > 0):
-                        taskData.args.add_arg("additional_file_size", len(file_additional.Content))
-                        taskData.args.add_arg("additional_file_raw", file_additional.Content)      
-            else: 
-                # hannibal don't lile null stuff, som I am here to please him :D
-                import os
-                taskData.args.add_arg("additional_file_size", 16)
-                taskData.args.add_arg("additional_file_raw", os.urandom(16))
-                raise Exception("Failed to get file contents, but don't worry I have other failsafe")
-        except Exception as e:
-            pass
+            if file_additional.Success:
+                if (len(file_additional.Content) > 0):
+                    taskData.args.add_arg("additional_file_size", len(file_additional.Content))
+                    taskData.args.add_arg("additional_file_raw", file_additional.Content)      
+        else: 
+            # hannibal don't lile null stuff, som I am here to please him :D
+            import os
+            taskData.args.add_arg("additional_file_size", 16)
+            taskData.args.add_arg("additional_file_raw", os.urandom(16))
+           
+      
             
         response.DisplayParams = ""
         
